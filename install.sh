249cd9368d2fcf91b22ca0d316afc791c620665b
nicejhonee <57075704+nicejhonee@users.noreply.github.com>
2025-11-18 22:50:13 +0100

    edit urls

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ remove=0
 
 update=0
 
-download_url="https://github.com/Jrohy/trojan/releases/download/"
+download_url="https://github.com/nicejhonee/trojan/releases/download/"
 
-version_check="https://api.github.com/repos/Jrohy/trojan/releases/latest"
+version_check="https://api.github.com/repos/nicejhonee/trojan/releases/latest"
 
-service_url="https://raw.githubusercontent.com/Jrohy/trojan/master/asset/trojan-web.service"
+service_url="https://raw.githubusercontent.com/nicejhonee/trojan/master/asset/trojan-web.service"
 
 [[ -e /var/lib/trojan-manager ]] && update=1
 
